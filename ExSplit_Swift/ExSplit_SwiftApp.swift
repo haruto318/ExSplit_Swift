9ea65577cb2eb3e5f0497a8bd90e297042e77a23
Haruto Hamano <haruto.yhs318@gmail.com>
2024-07-29 15:25:43 +0900

    Initial Commit

---
+++ ExSplit_Swift/ExSplit_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExSplit_SwiftApp.swift
+//  ExSplit_Swift
+//
+//  Created by 濱野遥斗 on 2024/07/29.
+//
+
+import SwiftUI
+
+@main
+struct ExSplit_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
