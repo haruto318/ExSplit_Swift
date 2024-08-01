38030a553d589ff0c8167f42dd6ee0f0950b286f
Haruto Hamano <haruto.yhs318@gmail.com>
2024-08-01 17:53:41 +0900

    add extensions of color and text style

---
+++ ExSplit_Swift/Extensions/ColorExtension.swift
@@ -0,0 +1,16 @@
+//
+//  ColorExtension.swift
+//  ExSplit_Swift
+//
+//  Created by 濱野遥斗 on 2024/08/01.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customFontColor = Color("FontColor")
+    static let customAccentColor = Color("AccentColor")
+    static let customButtonColor = Color("ButtonColor")
+    static let customFrameColor = Color("FrameColor")
+}
---
+++ ExSplit_Swift/Extensions/TextExtension.swift
@@ -0,0 +1,8 @@
+//
+//  TextExtension.swift
+//  ExSplit_Swift
+//
+//  Created by 濱野遥斗 on 2024/08/01.
+//
+
+import Foundation
