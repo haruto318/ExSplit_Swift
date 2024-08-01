a4469b2ede2d40d44c7dc33bd4eeda16323862ea
Haruto Hamano <haruto.yhs318@gmail.com>
2024-08-01 17:54:10 +0900

    create split cell in HOME

--- ExSplit_Swift/ContentView.swift
+++ ExSplit_Swift/ContentView.swift
@@ -10,11 +10,56 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Initail")
+            HStack(){
+                Text("ExSplit")
+                    .font(Font.custom("ZenMaruGothic-Medium", size: 24))
+                Spacer()
+            }
+            Divider()
+            
+            Button(action: {}) {
+                Text("グループを生成する")
+                    .fontStyle(.titleBold)
+            }
+            
+            VStack(){
+                
+                VStack(spacing: 5){
+                    HStack(){
+                        Text("東南アジア一周")
+                            .fontStyle(.headBold)
+                        Spacer()
+                    }
+                    VStack(spacing: 5){
+                        HStack(){
+                            Text("総金額")
+                                .fontStyle(.body)
+                                .foregroundColor(Color.customAccentColor)
+                            Spacer()
+                            Text("10,000 yen")
+                                .fontStyle(.body)
+                                .foregroundColor(Color.customFontColor)
+                        }
+                        HStack(){
+                            Text("参加人数")
+                                .fontStyle(.body)
+                                .foregroundColor(Color.customAccentColor)
+                            Spacer()
+                            Text("4人")
+                                .fontStyle(.body)
+                                .foregroundColor(Color.customFontColor)
+                        }
+                    }.padding(.horizontal)
+                }.padding(10)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(Color.customFrameColor, lineWidth: 1)
+                    )
+                
+            }.padding(.horizontal)
+            
         }
+        .padding(.horizontal ,10)
         .padding()
     }
 }
--- ExSplit_Swift/Extensions/TextExtension.swift
+++ ExSplit_Swift/Extensions/TextExtension.swift
@@ -6,3 +6,47 @@
 //
 
 import Foundation
+import SwiftUI
+
+enum FontStyle: String, CaseIterable {
+    case titleBold
+    case title
+    case head
+    case headBold
+    case body
+    case bodyBold
+    case description
+}
+
+extension FontStyle {
+    var dynamicType: SwiftUI.Font {
+        switch self {
+        case .titleBold:
+            return .custom("ZenMaruGothic-Medium", size: 20)
+
+        case .title:
+            return .custom("ZenMaruGothic-Regular", size: 20)
+            
+        case .head:
+            return .custom("ZenMaruGothic-Regular", size: 13)
+
+        case .headBold:
+            return .custom("ZenMaruGothic-Medium", size: 13)
+
+        case .body:
+            return .custom("ZenMaruGothic-Regular", size: 12)
+
+        case .bodyBold:
+            return .custom("ZenMaruGothic-Medium", size: 12)
+
+        case .description:
+            return .custom("ZenMaruGothic-Regular", size: 8)
+        }
+    }
+}
+
+extension Text {
+    func fontStyle(_ style: FontStyle) -> some View {
+        self.font(style.dynamicType)
+    }
+}
